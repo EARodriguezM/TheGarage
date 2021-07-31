2d307cbd7dd37ba561f493a0c92840061c288d81
EARodriguezM <esarodriguezme@gmail.com>
2021-07-31 13:13:35 -0500

    Oh shit! here we go again. New angular app skeleton. Add nebular and boostrap packages

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
-
-import { SharedModule } from "./shared/shared.module";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
 
 @NgModule({
   declarations: [
@@ -20,9 +17,7 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'default' }),
     NbLayoutModule,
-    NbEvaIconsModule,
-
-    SharedModule,
+    NbEvaIconsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
